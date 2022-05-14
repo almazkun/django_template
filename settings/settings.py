c15b6c52dcfabae3406331cb428d3391b0a7bd51
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-05-14 11:20:08 +0900

    STATICFILES_DIRS = [BASE_DIR / "static"]

--- settings/settings.py
+++ settings/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
